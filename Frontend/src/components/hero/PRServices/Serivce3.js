550bbe4b943dd7da85c17a102948973a21f76364
Alan Tom Jose <alantomjose@gmail.com>
2021-07-10 23:31:39 +0530

    marketing page

---
+++ Frontend/src/components/hero/PRServices/Serivce3.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+//eslint-disable-next-line
+import { css } from "styled-components/macro";
+
+import Header from "../../headers/light.js";
+
+import { ReactComponent as SvgDecoratorBlob1 } from "../../../images/svg-decorator-blob-1.svg";
+import DesignIllustration from "../../../images/design-illustration-2.svg";
+import CustomersLogoStripImage from "../../../images/customers-logo-strip.png";
+import BitcoinMain from "../../../images/Bitcoin1.png";
+import { Link, useHistory } from "react-router-dom";
+
+const Container = tw.div`relative`;
+const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
+const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left`;
+const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
+
+const Heading = tw.h1`font-bold text-2xl md:text-2xl lg:text-2xl xl:text-3xl text-gray-100 leading-tight`;
+const Paragraph = tw.p`my-5 lg:my-8 text-base text-gray-100 xl:text-lg`;
+
+const Actions = styled.div`
+  ${tw`relative max-w-md text-center mx-auto lg:-ml-1`}
+  input {
+    ${tw`sm:pr-48 pl-8 py-4 sm:py-5  text-gray-300 rounded-full bg-gray-900 border-2 w-full border-green-300 font-medium focus:outline-none transition duration-300  focus:border-green-500 hover:border-green-500`}
+  }
+  button {
+    ${tw`w-full  sm:absolute right-0 top-0 bottom-0 bg-green-600 text-gray-100 font-bold mr-2 my-4 sm:my-2 rounded-full py-4 flex items-center justify-center sm:w-40 sm:leading-none focus:outline-none hover:bg-green-800 transition duration-300`}
+  }
+`;
+
+const IllustrationContainer = tw.div`flex justify-center lg:justify-end items-center`;
+
+// Random Decorator Blobs (shapes that you see in background)
+const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
+  ${tw`pointer-events-none opacity-5 absolute left-0 bottom-0 h-64 w-64 transform -translate-x-2/3 -z-10`}
+`;
+
+const CustomersLogoStrip = styled.div`
+  ${tw`mt-12 lg:mt-20`}
+  p {
+    ${tw`uppercase text-sm lg:text-xs tracking-wider font-bold text-gray-100`}
+  }
+  img {
+    ${tw`mt-4 w-full lg:pr-16 xl:pr-32 opacity-100`}
+  }
+`;
+
+export default ({ roundedHeaderButton }) => {
+  const history = useHistory();
+  return (
+    <>
+      {/* <Header roundedHeaderButton={roundedHeaderButton} /> */}
+      <Container>
+        <TwoColumn>
+          <LeftColumn>
+            <Heading>Display Advertising</Heading>
+            <Paragraph>
+              PR and Media Outreach indispensable for any successful business.
+              At cfsquad, we aim to build solid connections with media world to
+              tailor the best story for your brand which increases a brand’s
+              credibility leading to more profit & sales to your business. We
+              create broadcast recognition campaigns to take your business to
+              the next level. So, grow your startup with customised and
+              affordable PR services created for specifically for young
+              businesses.
+            </Paragraph>
+          </LeftColumn>
+          <RightColumn>
+            <IllustrationContainer>
+              <img
+                tw="min-w-0 w-full max-w-lg xl:max-w-3xl"
+                src={BitcoinMain}
+                alt="Design Illustration"
+              />
+            </IllustrationContainer>
+          </RightColumn>
+        </TwoColumn>
+        <DecoratorBlob1 />
+      </Container>
+    </>
+  );
+};
---
+++ Frontend/src/demos/PRMarketingPage.js
@@ -0,0 +1,98 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "../helpers/AnimationRevealPage.js";
+import Hero from "../components/hero/PRTwoColumnWithInput.js";
+import Service1 from "../components/hero/PRServices/Serivce1";
+import Service2 from "../components/hero/PRServices/Serivce2";
+import Service3 from "../components/hero/PRServices/Serivce3";
+import Service4 from "../components/hero/PRServices/Serivce4";
+import Features from "../components/features/ThreeColWithSideImage.js";
+import MainFeature from "../components/features/TwoColWithButton.js";
+import MainFeature2 from "../components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+import FeatureWithSteps from "../components/features/TwoColWithSteps.js";
+import Pricing from "../components/pricing/ThreePlans.js";
+import Testimonial from "../components/testimonials/TwoColumnWithImageAndRating.js";
+import FAQ from "../components/faqs/SingleCol.js";
+import GetStarted from "../components/cta/GetStarted";
+import Footer from "../components/footers/FiveColumnWithBackground.js";
+// import heroScreenshotImageSrc from "../images/hero-screenshot-1.png";
+import macHeroScreenshotImageSrc from "../images/hero-screenshot-2.png";
+import stepsPic from "../images/stepsPic.png";
+import prototypeIllustrationImageSrc from "../images/prototype-illustration.svg";
+import valuespic from "../images/valuespic.png";
+import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
+import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
+import UserProfile from "../helper/auth/UserProfile.js";
+
+export default () => {
+  const Subheading = tw.span`uppercase tracking-widest font-bold text-gray-100`;
+  const HighlightedText = tw.span`text-green-300 `;
+  console.log(UserProfile.getRole());
+
+  return (
+    <AnimationRevealPage>
+      <Hero roundedHeaderButton={true} />
+      <Service1 roundedHeaderButton={true} />
+      <Service2 roundedHeaderButton={true} />
+      <Service3 roundedHeaderButton={true} />
+      <Service4 roundedHeaderButton={true} />
+
+      <div id="pricing"></div>
+
+      <Testimonial
+        subheading={<Subheading>Testimonials</Subheading>}
+        heading={
+          <>
+            Our Clients <HighlightedText>Love Us.</HighlightedText>
+          </>
+        }
+        testimonials={[
+          {
+            stars: 5,
+            profileImageSrc:
+              "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3.25&w=512&h=512&q=80",
+            heading: "Amazing User Experience",
+            quote:
+              "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+            customerName: "Charlotte Hale",
+            customerTitle: "Director, Delos Inc.",
+          },
+          {
+            stars: 5,
+            profileImageSrc:
+              "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=512&h=512&q=80",
+            heading: "Love the Developer Experience and Design Principles !",
+            quote:
+              "Sinor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+            customerName: "Adam Cuppy",
+            customerTitle: "Founder, EventsNYC",
+          },
+        ]}
+      />
+      <div id="contactus"></div>
+      <FAQ
+        subheading={<Subheading>FAQS</Subheading>}
+        heading={
+          <>
+            You have <HighlightedText>Questions ?</HighlightedText>
+          </>
+        }
+        faqs={[
+          {
+            question: "Which plan is the best for me ?",
+            answer:
+              "Yes, they all are. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          },
+          {
+            question: "Do I need to know anything before starting to invest ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          },
+        ]}
+      />
+      <GetStarted />
+      <Footer />
+    </AnimationRevealPage>
+  );
+};
